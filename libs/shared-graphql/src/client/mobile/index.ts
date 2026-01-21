4211fd819ef7911d3c2c2023bc00bce7b2aadd22
Atul Gupta <atul.gupta@guruq.in>
2026-01-21 23:07:48 +0530

    Remove @react-native-community/datetimepicker dependency and related code from the project. Update ApolloProvider usage in mobile applications to avoid context issues by using ApolloProvider directly instead of a wrapper component.

--- libs/shared-graphql/src/client/mobile/index.ts
+++ libs/shared-graphql/src/client/mobile/index.ts
@@ -5,15 +5,19 @@
  * It does not import web client to avoid Metro bundler parsing issues.
  * 
  * Usage:
- * import { GraphQLProvider, createApolloClient } from '@tutorix/shared-graphql/client/mobile';
+ * import { ApolloProvider } from '@apollo/client';
+ * import { apolloClient } from '@tutorix/shared-graphql/client/mobile';
  * 
  * // In your app root:
- * <GraphQLProvider>
+ * <ApolloProvider client={apolloClient}>
  *   <App />
- * </GraphQLProvider>
+ * </ApolloProvider>
+ * 
+ * NOTE: Mobile apps should use ApolloProvider directly (not a wrapper component)
+ * to avoid React Native context issues. The GraphQLProvider wrapper causes
+ * "Invalid hook call" and "Cannot read property 'useContext' of null" errors.
  */
 
 export * from './apollo-client';
-export * from './apollo-provider';
 // Shared utilities have been moved to platform-specific locations
 // Do NOT export from '../shared' to avoid Metro bundling web-specific code
